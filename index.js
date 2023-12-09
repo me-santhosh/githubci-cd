4cbf256d5bbcacb67a36426a65294d2d0d8a1374
me-santhosh <149153450+me-santhosh@users.noreply.github.com>
2023-12-09 19:52:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-
+  if (request.url === '/health') {
+    // Health Check Endpoint
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
+    response.end('OK');
+  } else if (request.url === '/readiness') {
+    // Readiness Check Endpoint
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end('OK');
+  } else {
+    // Your existing route or logic
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end('Hello World!');
+  }
 });
 
 var port = 80;
